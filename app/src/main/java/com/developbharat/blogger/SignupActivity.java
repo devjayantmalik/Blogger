b48c42b9417266f4d51b1c374947b72d5d829ad5
Jayant Malik <dev.jayantmalik@gmail.com>
2020-09-28 17:26:53 +0530

    added class methods for login and signup button

--- app/src/main/java/com/developbharat/blogger/SignupActivity.java
+++ app/src/main/java/com/developbharat/blogger/SignupActivity.java
@@ -2,7 +2,9 @@ package com.developbharat.blogger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void onBtnLoginClick(View view){
+        Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(i);
+    }
+
+    public void onBtnSignupClick(View view){
+
     }
 }
